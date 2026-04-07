e9a1dfe23f691dd47533b5f13f8a90d65e5e15ab
Saeed Rather <76421464+saeedyaqoob@users.noreply.github.com>
2026-04-07 10:12:42 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+  
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
