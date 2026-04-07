47cfa846610b078437c047ca6bf077f58f9f13cb
Saeed Rather <76421464+saeedyaqoob@users.noreply.github.com>
2026-04-07 10:33:16 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
-function myFunction() {
-  var dots1 = document.getElementById("dots1");
-  var moreText1 = document.getElementById("more1");
-  var btnText1 = document.getElementById("myBtn1");
-  
-  var dots2 = document.getElementById("dots2");
-  var moreText2 = document.getElementById("more2");
-  var btnText2 = document.getElementById("myBtn2");
+function myFunction1() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
-  if (dots1.style.display === "none") {
-    dots1.style.display = "inline";
-    btnText1.innerText = "Read more";
-    moreText1.style.display = "none";
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerText = "Read more";
+    moreText.style.display = "none";
   } else {
-    dots1.style.display = "none";
-    btnText1.innerText = "Read less";
-    moreText1.style.display = "inline";
+    dots.style.display = "none";
+    btnText.innerText = "Read less";
+    moreText.style.display = "inline";
   }
+}
 
-  if (dots2.style.display === "none") {
-    dots2.style.display = "inline";
-    btnText2.innerText = "Read more";
-    moreText2.style.display = "none";
+function myFunction2() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerText = "Read more";
+    moreText.style.display = "none";
   } else {
-    dots2.style.display = "none";
-    btnText2.innerText = "Read less";
-    moreText2.style.display = "inline";
+    dots.style.display = "none";
+    btnText.innerText = "Read less";
+    moreText.style.display = "inline";
   }
 }
